d66f71d1da850a095c192c32c974e509085d9136
roman <roman.yakhimovich@gmail.com>
2025-12-23 15:34:06 +0200

    I imported all needable libraries

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import java.awt.*
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import javax.swing.JPanel
+import javax.swing.Timer
+import kotlin.random.Random
